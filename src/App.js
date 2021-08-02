4c049b4bad220896b8b6a9a5fda5a597d19715a1
IhsenBen <benali.ihsen@gmail.com>
2021-08-03 00:59:50 +0200

    finish logic, passed test, needs styling

--- src/App.js
+++ src/App.js
@@ -1,57 +1,61 @@
 import "./App.css";
 import DrumPad from "./components/DrumPad";
-import React, { Component } from "react";
-import soundfile from './sounds/kick.mp3'
+import React, { useState, Component } from "react";
+
 
 const kit = [
   {
     key: "Q",
-    src: "https://github.com/IhsenBen/reactdrumsjs-808/blob/b3bf6f483f2f88b950bf54b04b526c42013a9fcd/src/sounds/bassdrum.WAV",
+    src: "https://github.com/IhsenBen/reactdrumsjs-808/blob/gh-pages/sounds/kick.mp3?raw=true",
     name: "Low Tom",
   },
   {
     key: "W",
-    src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    src: "https://github.com/IhsenBen/reactdrumsjs-808/blob/master/src/sounds/lowconga.WAV?raw=true",
     name: "Low Conga",
   },
   {
     key: "E",
-    src: {soundfile},
+    src: "https://github.com/IhsenBen/reactdrumsjs-808/blob/master/src/sounds/cowbell.WAV?raw=true",
     name: "Cow Bell",
   },
   {
     key: "A",
-    src: "./sounds/openhat.wav",
+    src: "https://github.com/IhsenBen/reactdrumsjs-808/blob/master/src/sounds/openhat.WAV?raw=true",
     name: "Open Hat",
   },
   {
     key: "S",
-    src: "./sounds/snaredrum.wav",
+    src: "https://github.com/IhsenBen/reactdrumsjs-808/blob/master/src/sounds/snaredrum.WAV?raw=true",
     name: "Snare Drum",
   },
   {
     key: "D",
-    src: "./sounds/lowtom.wav",
-    name: "Low Tom",
+    src: "https://github.com/IhsenBen/reactdrumsjs-808/blob/master/src/sounds/lowtom.WAV?raw=true",
+    name: "Low bla",
   },
   {
     key: "Z",
-    src: "./sounds/bassdrum.wav",
+    src: "https://github.com/IhsenBen/reactdrumsjs-808/blob/master/src/sounds/bassdrum.WAV?raw=true",
     name: "Bass Drum",
   },
   {
     key: "X",
-    src: "./sounds/midtown.wav",
+    src: "https://github.com/IhsenBen/reactdrumsjs-808/blob/master/src/sounds/midtown.WAV?raw=true",
     name: "Mid Town",
   },
   {
     key: "C",
-    src: "./sounds/handclap",
+    src: "https://github.com/IhsenBen/reactdrumsjs-808/blob/master/src/sounds/handclap.WAV?raw=true",
     name: "Hand Clap",
   },
 ];
-export default class App extends Component {
-  state = {
+export default class App extends Component {  constructor(props){
+  super(props)
+
+
+
+  this.state = {
     // I should replace this by an array of objects  with the key name, src of the audio file and the name of the audio
     // map through the array of objects
     // pass the elements as props in the app component
@@ -61,26 +65,34 @@ export default class App extends Component {
     // create a method that handle the display of the same of the sound on that component and  call in the above methods
     //call the handle click method on the drumpad div with onClick
     // voila voilou faut just coder tout ça bordel de merde/ i just need to code all this damn it
+    display:"hey!"
   };
+}
+
+
+
+  handleDisplay = display => this.setState({ display })
 
   render() {
     return (
       <div>
-        <div id="drum-machine" className="App">
+        <div className="App">
+          <div id="drum-machine" className="container">
           <div className="title">
-            <h1>Test react</h1>
+            <h1>Drum Machine</h1>
           </div>
 
           <div className="container">
-            <div className="drum-display">
-              <h1>Test Sound</h1>
+            <div  id="display" className="display">
+              <h1 id="display">{this.state.display}</h1>
             </div>
-            <div id="display" className="display">
+            <div  className="pads-container">
               {kit.map((sound, idx) => (
-                <DrumPad text={sound.key} key={sound.key} audio={sound.src} />
+                <DrumPad text={sound.key} key={sound.key} name={sound.name} audio={sound.src}  handleDisplay={this.handleDisplay}/>
               ))}
             </div>
           </div>
+        </div>
         </div>
       </div>
     );
--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -2,48 +2,62 @@ import "../App.css";
 
 import React from "react";
 
-
 export default class DrumPad extends React.Component {
-    constructor(props) {
-      super(props);
-      
-      this.audio = React.createRef();
-    }
-    
 
 
-  
-    
-     handleClick = () => {
-       console.log(this.audio)
-       this.audio.play()
-       this.audio.currentTime = 0
-      
-     }
-     
-    
-    render() {
-      const { text, audio } = this.props;
-      
-      return (
-        <div className="drum-pad" onClick={this.handleClick} id={`drum-${text}`}>
-          {text}
-          <audio  src={audio} className="clip"  ref={ref => this.audio = ref} id={text} />
-        </div>
-      )
-    }
-
-    
+//pass the event listener to the e function
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeydown);
+    window.focus();
   }
-      
-  document.addEventListener('keydown', (e) => {
+// close the event listener  
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeydown);
+  }
+// the above was like an open/close door to pass the key input ( not sure, but I think it's an outdated method)
 
-    const id = e.key.toUpperCase();
-    const audio = document.getElementById(id);
-    console.log(id);
-    if(audio) {
-      audio.currentTime = 0;
-      console.log(audio)
-      audio.play();
+  handleClick = () => {
+    console.log(this.audio);
+    this.audio.play();
+    this.audio.currentTime = 0;
+    this.props.handleDisplay(this.props.name);
+  };
+
+  handleKeydown = (e) => {
+    if (e.keyCode === this.props.text.charCodeAt()) {
+      this.audio.play();
+      this.audio.currentTime = 0;
+      this.props.handleDisplay(this.props.name);
     }
-  });
+  };
+
+  render() {
+    const { text, audio, name } = this.props;
+
+    return (
+      <div className="drum-pad" onClick={this.handleClick} id={`b-${text}`}>
+        {text}
+        <audio
+          src={audio}
+          className="clip"
+          ref={(ref) => (this.audio = ref)}
+          id={text}
+        />
+      </div>
+    );
+  }
+}
+
+// dosen't handle the display text state onchange
+// document.addEventListener('keydown', (e) => {
+
+//   const id = e.key.toUpperCase();
+//   const audio = document.getElementById(id);
+//   // console.log(id);
+//   if(audio) {
+//     audio.currentTime = 0;
+
+//     console.log(audio);
+//     audio.play();
+//   }
+// });
